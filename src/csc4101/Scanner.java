1e95e53d21b5f836c34884cd7b74182f5b750075
Alex Clavelle - i7thunder <aclav3113@gmail.com>
2014-09-23 13:05:35 -0500

    identifier validation helper functions

--- src/csc4101/Scanner.java
+++ src/csc4101/Scanner.java
@@ -1,5 +1,7 @@
 package csc4101;// Scanner.java -- the implementation of class Scanner
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 import java.io.*;
 
 class Scanner {
@@ -83,10 +85,15 @@ class Scanner {
             return new IdentToken(Character.toString(ch));
 
             // Identifiers
-        else if (ch >= 'A' && ch <= 'Z'
+        else if (isAlphabetic(ch)
          /* or ch is some other valid first character for an identifier */) {
             // TODO: scan an identifier into the buffer
 
+            char c = (char) tryRead(in);
+
+            while (c >= 'A' && c <= 'Z') {
+
+            }
 
             // put the character after the identifier back into the input
             // in->putback(ch);
@@ -110,5 +117,110 @@ class Scanner {
         return b;
     }
 
+    /**
+     * @param ident a char[] representing a potential identifier
+     * @return true if this char[] adheres to:
+     *
+        <identifier> --> <initial> <subsequent>*
+             | <peculiar identifier>
+        <initial> --> <letter> | <special initial>
+        <letter> --> a | b | c | ... | z
+
+        <special initial> --> ! | $ | % | & | * | / | : | < | =
+             | > | ? | ^ | _ | ~
+        <subsequent> --> <initial> | <digit>
+             | <special subsequent>
+        <digit> --> 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+        <special subsequent> --> + | - | . | @
+     *
+     * **/
+    private boolean isValidIdent(char[] ident) {
+        if (!isValidInitial(ident[0])) return false;
+        for (int i = 1; i < ident.length; i++) {
+            if (!isValidSubsequent(ident[i])) return false;
+        }
+        return true;
+    }
+
+    private boolean isValidInitial(char c) {
+        return (isAlphabetic(c) ||
+                isSpecialInitial(c)
+        );
+    }
+
+    private boolean isValidSubsequent(char c) {
+        return (isValidInitial(c)  ||
+                isNumber(c) ||
+                isSpecialSubsequent(c));
+    }
+
+    /**
+     * A-Z || a-z
+     */
+    private boolean isAlphabetic(char c) {
+        //          uppercase alphabet     lowercase alphabet
+        return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) ? true : false;
+    }
+
+    private boolean isNumber(char c) {
+        return (c >= '0' && c >= '9');
+    }
+
+    /**
+     * A special character which is valid in an identifier, but not at the beginning
+     */
+    private boolean isSpecialSubsequent(char c) {
+        return (c == '.' ||
+                c == '+' ||
+                c == '-' ||
+                c == '@');
+    }
+
+    /**
+     * A special character which is valid at any part of an identifier
+     */
+    private boolean isSpecialInitial(char c) {
+        return (c == '!' ||
+                c == '|' ||
+                c == '$' ||
+                c == '|' ||
+                c == '%' ||
+                c == '|' ||
+                c == '&' ||
+                c == '|' ||
+                c == '*' ||
+                c == '|' ||
+                c == '/' ||
+                c == '|' ||
+                c == ':' ||
+                c == '|' ||
+                c == '<' ||
+                c == '=' ||
+                c == '>' ||
+                c == '|' ||
+                c == '?' ||
+                c == '|' ||
+                c == '^' ||
+                c == '|' ||
+                c == '_' ||
+                c == '|' ||
+                c == '~');
+    }
+
+    private boolean isDelimiter(char c) {
+        return (c == ' ' ||
+                c == '(' ||
+                c == ')' ||
+                c == ';' ||
+                isQuote(c)
+        );
+    }
+
+    /**
+     * A quotation mark. '"' looks confusing.
+     */
+    private boolean isQuote(char c) {
+        return c == '"';
+    }
 
 }
